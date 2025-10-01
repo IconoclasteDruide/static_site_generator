724e74fc4d09209049c95c6be79eab6570cb2fce
André-Philippe Therrien <andre@accordancebible.com>
2025-10-01 04:22:06 -0600

    starting to add functionality for deployment to a real website

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
+
 from src.static_to_public import static_to_public
 from src.generate_pages_recursive import generate_pages_recursive
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = '/'
+
     static_to_public('static', 'public')
     generate_pages_recursive('content', 'template.html', 'public')
 
